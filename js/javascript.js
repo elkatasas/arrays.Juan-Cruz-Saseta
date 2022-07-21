bc98358e3b22fc6695ac1b4e3989e191a196c860
elkatasas <elkatasaseta28@gmail.com>
2022-07-21 01:01:25 -0300

    segundo int

--- js/javascript.js
+++ js/javascript.js
@@ -4,43 +4,71 @@ const Producto =[
         name:"CPU RYZEN 5 3400",
         precio: 50000,
     },
+
     {
-        id:2,
+        id: 2,
         name:"Monitor ReDragon 24",
         precio: 21000
     },
+
     {
-        id:3,
+        id: 3,
         name:"Gabinete Corsair FUSION",
         precio: 19000
     }
 ]
-
+const cart = [];
 
 
 
 
 let menu = "Bienvenido a sasTecnology\n 1- Selecciona un producto\n 2- Mi carrito\n 3- Elemininar elemento\n 0- salir";
+
     const addproducto = () =>
     {
         console.log("se agrega el producto");
         let list = "";
-        for(const elemento of Producto )
+        for (const element of Producto )
         {
-            //let listElemento = element\n;
-            list+= elemento.name;
-            let option = parseInt(prompt(list));
-            console.log(option);
+            list+= element.id+ "- "+ element.name +"\n";
         }
-        //let menu = "Bienvenido a sasTecnology\n 1- Selecciona un producto\n 2- Mi carrito\n 3- Elemininar elemento\n 0- salir";
+        let option = parseInt(prompt(list));
+        for (const interactor of Producto)
+        {
+            if(interactor.id == option)
+            {
+                let found = cart.find(element => element.id === option);
+                if(!found){
+                cart.push(interactor);
+                }
+            }
+        }
+        console.log(cart);
+        alert("Producto agregado con exito");
     }
     const showCart =() =>
     {
         console.log("se muestra el carrito");
+        let list = "";
+        let count = 1
+        for (const element of cart )
+        {
+            list+= count++ + "- "+ element.name +"\n";
+        }
+        alert(list);
     }
     const deletElement =() =>
     {
         console.log("se elimina el elmento");
+        let list = "";
+        let count = 1
+        for (const element of cart )
+        {
+            list+= count++ + "- "+ element.name +"\n";
+        }
+        let option =parseInt(prompt(list));
+        cart.splice(option - 1,1);
+        alert("eliminado con exito");
     }
 
 while(true)
